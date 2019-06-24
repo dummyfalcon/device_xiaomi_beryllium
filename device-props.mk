92a39c561bc9c2cc5b3f6f5544c4d338e6d1e5ef
Bladeyy <getsuga369@gmail.com>
2019-06-24 18:51:50 +0530

    beryllium: Enable USB debugging at boot
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- device-props.mk
+++ device-props.mk
@@ -5,3 +5,28 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display density
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=440
+
+# Display postprocessing
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.ppd.fde.config=0 \
+    vendor.display.enable_default_color_mode=1
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.fp.fpc=true \
+    ro.hardware.fp.goodix=true
+
+# FM
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.hw.fm.init=0
+
+# GNSS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.overlay.izat.optin=rro
+
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
